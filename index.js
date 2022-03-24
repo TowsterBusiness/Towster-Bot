4f9aa1f2b3d7a763ba48e4ccc3ac2c31f66ce141
Towster_ <towster.business@gmail.com>
2022-03-24 11:33:21 -0700

    Utils

--- index.js
+++ index.js
@@ -21,62 +21,63 @@ const playerUUID = "20c0adea-5d08-4ce9-9cbb-5e3e88ebd7dd"
 
 let apiData
 
-let weirdNamings = [
-    ['INK_SACK:3', 'INK_SACK'],
-    ['INK_SACK:4', 'ENCHANTED_INK_SACK'],
-    ['BAZAAR_COOKIE', 'BOOSTER_COOKIE'],
-    ['ENCHANTED_CARROT_ON_A_STICK', 'ENCHANTED_CARROT_STICK'],
-    ['RAW_FISH:1', 'RAW_FISH-1'],
-    ['RAW_FISH:2', 'RAW_FISH-2'],
-    ['RAW_FISH:3', 'RAW_FISH-3'],
-    ['LOG:1', 'LOG-1'],
-    ['LOG:2', 'LOG-2'],
-    ['LOG_2:1', 'LOG_2-1'],
-    ['LOG:3', 'LOG-3'],
-]
-
-let weirdNamings2 = [
-    ['INK_SACK', 'INK_SACK-3'],
-    ['ENCHANTED_INK_SACK', 'INK_SACK-4'],
-    ['BAZAAR_COOKIE', 'BOOSTER_COOKIE'],
-    ['ENCHANTED_CARROT_ON_A_STICK', 'ENCHANTED_CARROT_STICK'],
-    ['RAW_FISH:1', 'RAW_FISH-1'],
-    ['RAW_FISH:2', 'RAW_FISH-2'],
-    ['RAW_FISH:3', 'RAW_FISH-3'],
-    ['LOG:1', 'LOG-1'],
-    ['LOG:2', 'LOG-2'],
-    ['LOG_2:1', 'LOG_2-1'],
-    ['LOG:3', 'LOG-3'],
-]
-
-const readFile = function(path) {
-    let apiData = fs.readFileSync(path, "utf-8", (err, data) => {
-        if (err) { console.log(err) }
-        apiData = data;
-    })
-    return JSON.parse(apiData)
-}
-
-const bazzarSellOffer = function(object) {
-    return apiData.products[object].buy_summary[0].pricePerUnit
-    
-}
-
-const bazzarBuyOffer = function(object) {
-    return apiData.products[object].sell_summary[0].pricePerUnit
-}
-
-const bazzarBuyAnount = function(object) {
-    return apiData.products[object].quick_status.buyVolume
-}
-
-const bazzarSellAmount = function(object) {
-    return apiData.products[object].quick_status.sellVolume
+const Utils = {
+    readFile: function(path) {
+        let apiData = fs.readFileSync(path, "utf-8", (err, data) => {
+            if (err) { console.log(err) }
+            apiData = data;
+        })
+        return JSON.parse(apiData)
+    },
+    bazzarSellOffer: function(object) {
+        return apiData.products[object].buy_summary[0].pricePerUnit
+    },
+    bazzarBuyOffer: function(object) {
+        return apiData.products[object].sell_summary[0].pricePerUnit
+    },
+    bazzarBuyAnount: function(object) {
+        return apiData.products[object].quick_status.buyVolume
+    },
+    bazzarSellAmount: function(object) {
+        return apiData.products[object].quick_status.sellVolume
+    },
+    fetchBazzarAPI: async function() {
+        const apiData = await fetch('https://api.hypixel.net/skyblock/bazaar')
+            .then(data => data.json())
+            .then(data => {return data});
+        return apiData
+    },
+    weirdNamings: [
+        ['INK_SACK:3', 'INK_SACK'],
+        ['INK_SACK:4', 'ENCHANTED_INK_SACK'],
+        ['BAZAAR_COOKIE', 'BOOSTER_COOKIE'],
+        ['ENCHANTED_CARROT_ON_A_STICK', 'ENCHANTED_CARROT_STICK'],
+        ['RAW_FISH:1', 'RAW_FISH-1'],
+        ['RAW_FISH:2', 'RAW_FISH-2'],
+        ['RAW_FISH:3', 'RAW_FISH-3'],
+        ['LOG:1', 'LOG-1'],
+        ['LOG:2', 'LOG-2'],
+        ['LOG_2:1', 'LOG_2-1'],
+        ['LOG:3', 'LOG-3'],
+    ],
+    weirdNamings2: [
+        ['INK_SACK', 'INK_SACK-3'],
+        ['ENCHANTED_INK_SACK', 'INK_SACK-4'],
+        ['BAZAAR_COOKIE', 'BOOSTER_COOKIE'],
+        ['ENCHANTED_CARROT_ON_A_STICK', 'ENCHANTED_CARROT_STICK'],
+        ['RAW_FISH:1', 'RAW_FISH-1'],
+        ['RAW_FISH:2', 'RAW_FISH-2'],
+        ['RAW_FISH:3', 'RAW_FISH-3'],
+        ['LOG:1', 'LOG-1'],
+        ['LOG:2', 'LOG-2'],
+        ['LOG_2:1', 'LOG_2-1'],
+        ['LOG:3', 'LOG-3'],
+    ]
 }
 
 const calculateCraft = async function(craftItem) {
-    const itemData = readFile(`./NEU-REPO/items/${craftItem}.json`)
-    const unidefinedItemList = readFile('./bazzar_Item_List.json')
+    const itemData = Utils.readFile(`./NEU-REPO/items/${craftItem}.json`)
+    const unidefinedItemList = Utils.readFile('./bazzar_Item_List.json')
     let slotNames = [
         'A1',
         'A2',
@@ -93,9 +94,9 @@ const calculateCraft = async function(craftItem) {
     for (slot in slotNames) {
         if (itemData.recipe == null) return
         let item = itemData.recipe[slotNames[slot]].split(':')
-        for (wierdName in weirdNamings2) {
-            if (item[0] == weirdNamings2[wierdName][1])
-                item[0] = weirdNamings2[wierdName][0]
+        for (wierdName in Utils.weirdNamings2) {
+            if (item[0] == Utils.weirdNamings2[wierdName][1])
+                item[0] = Utils.weirdNamings2[wierdName][0]
         }
         let alreadyInCraft = false
         for (index in craft) {
@@ -106,7 +107,7 @@ const calculateCraft = async function(craftItem) {
         }
         if (!alreadyInCraft && item[0] != '') {
             try {
-                craft.push([item[0], parseInt(item[1]), bazzarSellAmount(item[0])])
+                craft.push([item[0], parseInt(item[1]), Utils.bazzarSellAmount(item[0])])
             } catch {
                 craft.push([item[0], parseInt(item[1]), 'Not Bazzar Buyable'])
             }
@@ -116,8 +117,8 @@ const calculateCraft = async function(craftItem) {
 }
 
 const calculateFlip = async function(flipItem) {
-    const itemData = readFile(`./NEU-REPO/items/${flipItem}.json`)
-    const unidefinedItemList = readFile('./bazzar_Item_List.json')
+    const itemData = Utils.readFile(`./NEU-REPO/items/${flipItem}.json`)
+    const unidefinedItemList = Utils.readFile('./bazzar_Item_List.json')
     let buyPrice = 0;
     let slotNames = [
         'A1',
@@ -140,12 +141,12 @@ const calculateFlip = async function(flipItem) {
             
         if (itemData.recipe[slotNames[slot]] != '')          
         {
-            for (wierdName in weirdNamings2) {
-                if (item[0] == weirdNamings2[wierdName][1])
-                    item[0] = weirdNamings2[wierdName][0]
+            for (wierdName in Utils.weirdNamings2) {
+                if (item[0] == Utils.weirdNamings2[wierdName][1])
+                    item[0] = Utils.weirdNamings2[wierdName][0]
             }
             try {
-                buyPrice += bazzarBuyOffer(item[0]) * item[1]
+                buyPrice += Utils.bazzarBuyOffer(item[0]) * item[1]
             } catch {
                 if (unidefinedItemList[item[0]] != null) {
                     buyPrice += unidefinedItemList[item[0]]
@@ -160,24 +161,17 @@ const calculateFlip = async function(flipItem) {
     return apiData.products[flipItem].buy_summary[0].pricePerUnit - buyPrice
 }
 
-const fetchBazzarAPI = async function() {
-    const apiData = await fetch('https://api.hypixel.net/skyblock/bazaar')
-        .then(data => data.json())
-        .then(data => {return data});
-    return apiData
-}
-
 const main = async function() {
-    apiData = await fetchBazzarAPI();
+    apiData = await Utils.fetchBazzarAPI();
 
     let bazzarItemList = [];
     for (value in apiData.products) {
-        for (wierdName in weirdNamings) {
-            if (value == weirdNamings[wierdName][0])
-                value = weirdNamings[wierdName][1]
+        for (wierdName in Utils.weirdNamings) {
+            if (value == Utils.weirdNamings[wierdName][0])
+                value = Utils.weirdNamings[wierdName][1]
         }
         let flipValue = await calculateFlip(value)
-        if (flipValue != 'Not Craftable' && flipValue > 0 && bazzarBuyAnount(value) > 2000) {
+        if (flipValue != 'Not Craftable' && flipValue > 0 && Utils.bazzarBuyAnount(value) > 2000) {
             let craft = await calculateCraft(value);
             let highestTimeToBuy = 0;
             for (item in craft) {
@@ -185,7 +179,7 @@ const main = async function() {
                     highestTimeToBuy = craft[item][2]/craft[item][1]
                 }
             }
-            bazzarItemList.push([Math.floor(flipValue * highestTimeToBuy), bazzarBuyAnount(value), value, Math.floor(flipValue)])
+            bazzarItemList.push([Math.floor(flipValue * highestTimeToBuy), Utils.bazzarBuyAnount(value), value, Math.floor(flipValue)])
         }
             
     }
